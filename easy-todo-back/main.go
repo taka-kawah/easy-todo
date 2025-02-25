0eef7c497ee262f417ad1e77e9bf6be24aaac482
taka-kawah <kawaharatakashi0724@gmail.com>
2025-02-25 22:11:26 +0900

    ginでルーティング

--- easy-todo-back/main.go
+++ easy-todo-back/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"easy-todo-back/db"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -12,7 +15,55 @@ func main() {
 	}
 	defer db.DisconnectToDb(sqldb)
 
-	targetId := 2633542081492844029
+	r := gin.Default()
 
-	db.DeleteTodoById(dbInstance, int64(targetId))
+	r.POST("/api/v1/addnote", func(ctx *gin.Context) {
+		val := ctx.Query("val")
+		db.CreateToDo(dbInstance, val)
+		ctx.JSON(200, gin.H{"message": "added!"})
+	})
+
+	r.GET("api/v1/alltodos", func(ctx *gin.Context) {
+		todos := db.ReadToDo(dbInstance)
+		ctx.JSON(200, todos)
+	})
+
+	r.GET("/api/v1/singletodo", func(ctx *gin.Context) {
+		key, err := strconv.ParseInt(ctx.Query("key"), 10, 64)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "error"})
+		}
+		todo := db.ReadSingleTodoById(dbInstance, key)
+		ctx.JSON(200, todo)
+	})
+
+	r.PUT("/api/v1/rewrite", func(ctx *gin.Context) {
+		key, err := strconv.ParseInt(ctx.Query("key"), 10, 64)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "error"})
+		}
+		newVal := ctx.Query("newVal")
+		db.UpDateTodoValById(dbInstance, key, newVal)
+		ctx.JSON(200, gin.H{"message": "updated!"})
+	})
+
+	r.PUT("/api/v1/check", func(ctx *gin.Context) {
+		key, err := strconv.ParseInt(ctx.Query("key"), 10, 64)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "error"})
+		}
+		db.CheckTodoById(dbInstance, key)
+		ctx.JSON(200, gin.H{"message": "checked!"})
+	})
+
+	r.DELETE("/api/v1/delete", func(ctx *gin.Context) {
+		key, err := strconv.ParseInt(ctx.Query("key"), 10, 64)
+		if err != nil {
+			ctx.JSON(500, gin.H{"message": "error"})
+		}
+		db.DeleteTodoById(dbInstance, key)
+		ctx.JSON(200, gin.H{"message": "deleted!"})
+	})
+
+	r.Run()
 }
